58199cf986f921b021a9649e1bd9acc38ef0f6fe
Karol Sulżyński <karolkonsulat@gmail.com>
2023-10-28 16:41:42 +0200

    użycie continue

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        /*Zadanie 1*/
+
+       /*for(int i = 0; i <= 20; i++) {
+           if (i % 2 != 1)
+               continue;
+           System.out.println(i);
+       }
+    }
+}*/
+
+        /*Zadanie 2*/
+        int i = 0;
+        while (i++ <= 20) {
+
+            {if (i % 2 != 1) continue;
+                    System.out.println(i);
+                }
+            }
+        }
+    }
